dbfcc9316f94cdd0fe5a79c8ef98ceb6023e43a2
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 11:29:31 -0600

    Update consola.php

--- consola.php
+++ consola.php
@@ -1,4 +1,4 @@
 <?php       
        //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
-       echo exec('php composer.phar require "mercadopago/dx-php"');
+       echo exec('php -r "copy("https://getcomposer.org/installer", "composer-setup.php");"');
 ?>
