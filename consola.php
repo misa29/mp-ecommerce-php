ebdd5821798cc7be8bfd9d2ccaba8bd7ced54c21
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 11:40:07 -0600

    Update consola.php

--- consola.php
+++ consola.php
@@ -1,4 +1,4 @@
 <?php       
        //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
-     echo exec('php composer.phar require "mercadopago/dx-php"');
+     echo exec('composer require "mercadopago/dx-php:2.1.1"');
 ?>
