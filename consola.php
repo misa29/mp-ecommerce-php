b2c844f66789ca0be1865bf695077c3af240c0af
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 11:31:20 -0600

    Update consola.php

--- consola.php
+++ consola.php
@@ -1,4 +1,7 @@
 <?php       
        //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
        echo exec('php -r "copy("https://getcomposer.org/installer", "composer-setup.php");"');
+       echo exec("php -r 'if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;'");
+       echo exec('php composer-setup.php');
+       echo exec("php -r 'unlink('composer-setup.php');'");
 ?>
