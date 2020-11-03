d0c7c2f3fcff24d14f58ccaa4d4ba1f07b447aa8
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 11:17:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php       
        //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
-       echo exec("php -r 'if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;'");
+       echo exec("php composer-setup.php");
+       echo exec("php -r 'unlink('composer-setup.php');'");
 ?>
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
