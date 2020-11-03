359bfe7bf19b097c687c72089a3574ea6542d9ea
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 10:54:25 -0600

    Create consola.php

---
+++ consola.php
@@ -0,0 +1,4 @@
+<?php       
+       //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
+       echo exec('php composer.phar require "mercadopago/dx-php"');
+?>
