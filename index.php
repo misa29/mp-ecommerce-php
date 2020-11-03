69de2e66e1007ca4dc801a839777cfa9768091f4
misa29 <73900419+misa29@users.noreply.github.com>
2020-11-03 11:21:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php       
-       //echo "THis is a test file on a test repo for testing the deploy functionality using github webhooks!!!!";
-       echo exec("php composer-setup.php");
-    //   echo exec("php -r 'unlink('composer-setup.php');'");
+       //echo "THis is a test file on a test repo for testing the deplo y functionality using github webhooks!!!!";
+    echo exec('php composer.phar require "mercadopago/dx-php"');
 ?>
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
